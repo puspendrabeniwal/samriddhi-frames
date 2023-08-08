a2c5df6d48411e2abc8fef3064ff4f3414aa09e5
Puspendra Beniwal <beniwalpuspendra>
2023-08-08 18:45:21 +0530

    contact us api with pwd

--- config/global_constant.js
+++ config/global_constant.js
@@ -1,5 +1,5 @@
 /** Website root directory path */
-WEBSITE_ROOT_PATH	=(process.env.PWD) ? process.env.PWD : process.cwd()+"/"
+WEBSITE_ROOT_PATH	= (process.env.PWD) ? process.env.PWD+"/" : process.cwd()+"/"
 
 /** Website Push notification server key for Android devices */
 WEBSITE_PN_ANDROID_SERVER_KEY =	process.env.ANDROID_SERVER_KEY;
